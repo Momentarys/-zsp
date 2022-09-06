08e2f2193d17f099c9644c55e4f60b0eb2f0f1cb
zsp <css@qq.con>
2022-09-06 16:13:36 +0800

    feat:postcss配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>黑马头条</div>
+  <div>
+    <span class="toutiao toutiao-gengduo"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box"></div>
+  </div>
 </template>
 
 <script>
+// 下载vant
+// yarn add vant@latest-v2 -S
+
+// rem适配
+// 1.让作m动起来
+// yarn add amfe-flexible
+// 2.盒子使用rem
+// 自动识别css中的px，并将其转成rem
+
+// post-css css的预处理器
+// postcss 自己能干的事很少，下载插件
+
+// 1.下载 yarn add postcss-pxtorem
+// 2.告诉postcss,我要使用这个插件，.postcssrc.js
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+.box {
+  width: 700px;
+  height: 150px;
+  background-color: deeppink;
+}
+</style>
